4353e9240c350fdea7f6f5cee7dffb14ae927867
pswanagan <pswanagan@gmail.com>
2024-02-14 14:05:35 -0600

    finished intial backend

--- models/animal.js
+++ models/animal.js
@@ -2,16 +2,28 @@ const {Schema, model} = require('../config/db-connection')
 
 
 const animalSchema = Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true
-  }
+  id: {type: Number,
+  required: true},
+  name: {type: String,
+  required: true},
+  species: {type: String,
+    required: true},
+  family: {type: String,
+    required: true},
+  habitat: {type: String,
+    required: true},
+  place_of_found: {type: String,
+    required: true},
+  diet: {type: String,
+    required: true},
+  description: {type: String,
+    required: true},
+  weight_kg: {type: Number,
+    required: true},
+  height_cm: {type: Number,
+    required: true},
+  image: String,
+  keepers:[String] 
 })
 
 // You can add pre hooks to your schema right here
